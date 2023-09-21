06848b06f02e4b9309a3d917225e000ccd2ba79a
Asus <dasa1088@gmail.com>
2023-09-21 04:06:52 -0500

    solution exercise 1

--- exercise1.py
+++ exercise1.py
@@ -1,3 +1,4 @@
+# Import libraries
 import pandas as pd
 
 # Create dataframe
@@ -5,5 +6,13 @@ flight = pd.read_csv('src/flight_source.csv'\
                      , delimiter=";"\
                      , parse_dates=["actl_dep_lcl_tms", "actl_arr_lcl_tms", "airborne_lcl_tms", "landing_lcl_tms"])
 
-print(flight.dtypes)
+# print(flight.dtypes)
 
+# Create next_flight_id column using lead window function
+flight["next_flight_id"] = flight.sort_values(["acft_regs_cde", "actl_dep_lcl_tms"], ascending=[True, True])\
+                                                .groupby(["acft_regs_cde"])["id"]\
+                                                    .shift(-1)
+
+flight["next_flight_id"] = flight["next_flight_id"].astype(pd.Int32Dtype())
+
+print(flight)
