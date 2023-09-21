7548f09ec04ed5976c7dfac7514638669643a7b5
Asus <dasa1088@gmail.com>
2023-09-21 04:27:44 -0500

    solution exercise 2

---
+++ exercise2.py
@@ -0,0 +1,10 @@
+# Import libraries
+import pandas as pd
+
+# Create dataframe
+flight = pd.read_csv('src/flight_source.csv'\
+                     , delimiter=";"\
+                     , parse_dates=["actl_dep_lcl_tms", "actl_arr_lcl_tms", "airborne_lcl_tms", "landing_lcl_tms"])
+
+# print(flight.dtypes)
+
